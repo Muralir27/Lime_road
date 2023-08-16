974aec350e86f59e89c2fa1b30752b0c7c554be7
Muralir27 <muralir270197@gmail.com>
2023-08-16 11:58:20 +0530

    Launch modified

--- src/test/java/com/Human/MyProject/Launch.java
+++ src/test/java/com/Human/MyProject/Launch.java
@@ -12,10 +12,10 @@ public class Launch {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver =new ChromeDriver();
 		driver.get("https://www.nopcommerce.com/register");
-		driver.manage().window().maximize();
 		System.out.println("Title of the WEBPAGE :"+driver.getTitle());
 		System.out.println("CurrentURL of the WEBPAGE :"+driver.getCurrentUrl());
 		System.out.println(driver.getPageSource());
+		driver.manage().window().maximize();
 	}
 
 }
